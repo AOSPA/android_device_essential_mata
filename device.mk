d713e24aae575cf583f18b9af1577914bbb02fcf
Nolen Johnson <johnsonnolen@gmail.com>
2020-11-23 14:46:29 -0500

    mata: Cleanup dead targets
    Offending entries:
    copybit.msm8998
    ebtables
    ethertypes
    libQWiFiSoftApCfg
    libicuuc.vendor
    liboverlay
    move_widevine_data.sh
    textclassifier.smartselection.bundle1
    build/make/core/main.mk:1136: error: Build failed.
    
    Change-Id: I0e4d93f4c3be077d49befa1f547d11d623ca74d5

--- device.mk
+++ device.mk
@@ -131,11 +131,9 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    copybit.msm8998 \
     gralloc.msm8998 \
     hwcomposer.msm8998 \
     libdisplayconfig \
-    liboverlay \
     libqdMetaData.system \
     libtinyxml \
     libvulkan \
@@ -145,8 +143,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey \
-    move_widevine_data.sh
+    android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
@@ -193,11 +190,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
-
-# IPv6 tethering
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes
 
 # IRQ Balancing
 PRODUCT_COPY_FILES += \
@@ -357,10 +349,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
-# TextClassifier
-PRODUCT_PACKAGES += \
-    textclassifier.smartselection.bundle1
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.pixel
@@ -404,11 +392,9 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 # Update this list with what each blob is actually for
-# libicuuc: vendor.qti.hardware.qteeconnector@1.0-impl
 # libstdc++: hexagon DSP blobs
 PRODUCT_EXTRA_VNDK_VERSIONS := 29
 PRODUCT_PACKAGES += \
-    libicuuc.vendor \
     libstdc++.vendor \
     vndk_package
 
