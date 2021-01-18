2916e716e34678d59bed94144521ece83c5aaae1
Anay Wadhera <awadhera@berkeley.edu>
2021-01-18 00:26:21 -0800

    mata: don't build qti power by default

--- device.mk
+++ device.mk
@@ -300,6 +300,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
 # Power
+TARGET_PROVIDES_POWERHAL := true
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr \
     android.hardware.power.stats@1.0-service.pixel
