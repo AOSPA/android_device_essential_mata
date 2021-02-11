52c5223718265d77a5a16dd60c1f6ae6d1f668b3
Alexander Koskovich <zvnexus@outlook.com>
2021-02-11 16:09:00 -0700

    mata: Allign extract utilities with recent template.
    Change-Id: Icbdd93f95f4a8a078099316beca7b7f0ac579b31

--- extract-files.sh
+++ extract-files.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -21,57 +11,72 @@ set -e
 DEVICE=mata
 VENDOR=essential
 
-# Load extract_utils and do some sanity checks
+# Load extract utilities and do some sanity checks.
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-PA_ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="$PA_ROOT"/vendor/pa/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-source "$HELPER"
+source "${HELPER}"
 
-# Default to sanitizing the vendor folder before extraction
+# Default to sanitizing the vendor folder before extraction.
 CLEAN_VENDOR=true
-SECTION=
-KANG=
 
-while [ "$1" != "" ]; do
-    case "$1" in
-        -n | --no-cleanup )     CLEAN_VENDOR=false
-                                ;;
-        -k | --kang)            KANG="--kang"
-                                ;;
-        -s | --section )        shift
-                                SECTION="$1"
-                                CLEAN_VENDOR=false
-                                ;;
-        * )                     SRC="$1"
-                                ;;
+KANG=
+SECTION=
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
     esac
     shift
 done
 
 if [ -z "${SRC}" ]; then
-    SRC=adb
+    SRC="adb"
 fi
 
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$PA_ROOT" false "$CLEAN_VENDOR"
+function blob_fixup() {
+    case "${1}" in
+        vendor/bin/imsrcsd|vendor/lib64/lib-imsrcs-v2.so|vendor/lib64/lib-uceservice.so)
+            patchelf --add-needed "libbase_shim.so" "${2}"
+            ;;
+        vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.mata.rc)
+            sed -i 's/service fps_hal_mata/service vendor.fps_hal_mata/g' "${2}"
+            ;;
+        vendor/etc/init/vendor.essential.hardware.sidecar@1.0-service.rc)
+            sed -i 's/service sidecar-hal-1-0/service vendor.sidecar-hal-1-0/g' "${2}"
+            ;;
+        vendor/lib/hw/vulkan.msm8998.so|vendor/lib64/hw/vulkan.msm8998.so)
+            patchelf --set-soname "vulkan.msm8998.so" "${2}"
+            ;;
+        vendor/lib64/libgf_ud_hal.so|vendor/lib64/libgf_g6_ud_hal.so)
+            sed -i "s|vendor.boot.verifiedbootstate|vendor.boot.fingerprintbstate|g" "${2}"
+            ;;
+    esac
+}
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC" ${KANG} --section "$SECTION"
-extract "$MY_DIR"/proprietary-files-recovery.txt "$SRC" ${KANG} --section "$SECTION"
+# Initialize the helper.
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-sed -i 's/service fps_hal_mata/service vendor.fps_hal_mata/g' "$PA_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.mata.rc
-sed -i 's/service sidecar-hal-1-0/service vendor.sidecar-hal-1-0/g' "$PA_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/etc/init/vendor.essential.hardware.sidecar@1.0-service.rc
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files-recovery.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
-patchelf --add-needed "libbase_shim.so" "$PA_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/bin/imsrcsd
-patchelf --add-needed "libbase_shim.so" "$PA_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/lib-imsrcs-v2.so
-patchelf --add-needed "libbase_shim.so" "$PA_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/lib-uceservice.so
-patchelf --set-soname "vulkan.msm8998.so" "$PA_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/hw/vulkan.msm8998.so
-patchelf --set-soname "vulkan.msm8998.so" "$PA_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/hw/vulkan.msm8998.so
-
-"$MY_DIR"/setup-makefiles.sh
+"${MY_DIR}/setup-makefiles.sh"
