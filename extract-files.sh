60801d7bcefdbd4a9f665f3d1984a7f7f4fe261e
Anay Wadhera <awadhera@berkeley.edu>
2021-03-30 16:04:11 -0700

    mata: move to versioned patchelf
    Change-Id: I4e0a5cdc3bba489af905a45b9506a4fc8ae0ac94

--- extract-files.sh
+++ extract-files.sh
@@ -56,7 +56,7 @@ fi
 function blob_fixup() {
     case "${1}" in
     vendor/bin/imsrcsd|vendor/lib64/lib-imsrcs-v2.so|vendor/lib64/lib-uceservice.so)
-        patchelf --add-needed "libbase_shim.so" "${2}"
+        ${PATCHELF} --add-needed "libbase_shim.so" "${2}"
         ;;
     vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.mata.rc)
         sed -i 's/service fps_hal_mata/service vendor.fps_hal_mata/g' "${2}"
@@ -65,7 +65,7 @@ function blob_fixup() {
         sed -i 's/service sidecar-hal-1-0/service vendor.sidecar-hal-1-0/g' "${2}"
         ;;
     vendor/lib/hw/vulkan.msm8998.so|vendor/lib64/hw/vulkan.msm8998.so)
-        patchelf --set-soname "vulkan.msm8998.so" "${2}"
+        ${PATCHELF} --set-soname "vulkan.msm8998.so" "${2}"
         ;;
     esac
 }
